6c2fd0e054582babd45b31eb37e4c1c50c73bd42
longthk17 <kimlong.speed8993@gmail.com>
2024-06-10 17:45:15 +0700

    feat: add learn git channels

---
+++ mysite/asgi.py
@@ -0,0 +1,23 @@
+"""
+ASGI config for mysite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": asgi_application,
+    }
+)
